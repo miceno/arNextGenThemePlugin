1eb0e4dc42b5d8b7677fbb6ce4d9ad63b4e2ce04
Orestes Sanchez <orestes.sanchezbenavente@telefonica.com>
2023-06-03 01:50:25 +0200

    Initial code.

---
+++ templates/_layout_start.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="<?php echo $sf_user->getCulture(); ?>" dir="<?php echo sfCultureInfo::getInstance($sf_user->getCulture())->direction; ?>">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include_title(); ?>
+    <?php echo get_component('default', 'tagManager', ['code' => 'script']); ?>
+    <link rel="shortcut icon" href="<?php echo public_path('favicon.ico'); ?>">
+    <script defer src="/dist/js/vendor.bundle.73b19f4e2ba6b6172f75.js"></script><script defer src="/dist/js/arNextGenThemePlugin.bundle.1af1104098006032615f.js"></script>
+    <?php echo get_component_slot('css'); ?>
+  </head>
+  <body class="d-flex flex-column min-vh-100 <?php echo $sf_context->getModuleName(); ?> <?php echo $sf_context->getActionName(); ?>">
+    <?php echo get_component('default', 'tagManager', ['code' => 'noscript']); ?>
+    <?php echo get_partial('header'); ?>
+    <?php include_slot('pre'); ?>
