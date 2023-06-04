9e4211bc0e2047a8dcefbcb38d80867bdcf0aa15
Orestes Sanchez <orestes.sanchezbenavente@telefonica.com>
2023-06-05 00:55:06 +0200

    Remove no overriden templates.

--- modules/user/templates/_showActions.php
+++
@@ -1,5 +0,0 @@
-<?php if ($sf_context->getConfiguration()->isPluginEnabled('arCasPlugin')) { ?>
-    <?php include '_showActions.mod_cas.php'; ?>
-<?php } else { ?>
-    <?php include '_showActions.mod_standard.php'; ?>
-<?php } ?>
