3ba49046b49d69df8128570e4e183d4e6c99a25e
Orestes Sanchez <orestes.sanchezbenavente@telefonica.com>
2023-06-15 00:46:06 +0200

    Show arrows of carousel on dark.

--- modules/staticpage/templates/homeSuccess.php
+++ modules/staticpage/templates/homeSuccess.php
@@ -120,11 +120,11 @@
                 </a>
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselHomePage" data-bs-slide="prev">
+        <button class="carousel-control-prev carousel-dark" type="button" data-bs-target="#carouselHomePage" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselHomePage" data-bs-slide="next">
+        <button class="carousel-control-next carousel-dark" type="button" data-bs-target="#carouselHomePage" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
